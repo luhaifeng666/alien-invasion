159fa4ce895c178eafa49db31cb91af9548165d7
luhaifeng666 <youzui@hotmail.com>
2023-11-29 23:30:53 +0800

    performance: 重构

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,8 @@
 '''
 Author: luhaifeng666 youzui@hotmail.com
 Date: 2023-11-09 16:29:32
-LastEditors: luhaifeng666 youzui@hotmail.com
-LastEditTime: 2023-11-13 14:15:46
+LastEditors: luhaifeng666
+LastEditTime: 2023-11-29 23:29:04
 FilePath: /alien-invasion/alien_invasion.py
 Description: 
 
@@ -28,17 +28,21 @@ class AlienInvasion:
 
     def run_game(self):
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._unpdate_screen()
 
-            # 每次循环时都重绘屏幕
-            self.screen.fill(self.settings.bg_color)
-
-            self.ship.blitme()
-
-            pygame.display.flip()
-
+    def _check_events(self):
+      # 响应按键和鼠标事件
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          sys.exit()
+          
+    def _unpdate_screen(self):
+      # 更新屏幕上的颜色，并切换到新屏幕
+      self.screen.fill(self.settings.bg_color)
+      self.ship.blitme()
+      
+      pygame.display.flip()
 
 if __name__ == '__main__':
     ai = AlienInvasion()
